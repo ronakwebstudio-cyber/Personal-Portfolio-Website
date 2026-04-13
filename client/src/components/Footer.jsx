4e4b0e899ba2fb75743a77f526aff148cf19ffcb
Ronak Pradhan <ronak.webstudio@email.com>
2026-04-13 20:49:56 +0530

    huu

--- client/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer(){
-  return (
-    <footer className="footer">
-      <span className="footer-copy">© All rights reserved.</span>
-    </footer>
-  )
-}
